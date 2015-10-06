84d5b0bd8b478a4873a9ac1398c64e2474e482b7
Student <student@RU-009.local>
2015-10-06 08:58:32 -0600

    Node 2 Bonus

--- Node2/routes/index.js
+++ Node2/routes/index.js
@@ -4,10 +4,24 @@
  */
 
 var fs = require('fs');  
-var fileContents = fs.readFileSync('data.txt', 'utf-8');
+var express = require('express'); 
+
+var app = express(); 
 
 
 exports.index = function(req, res){
-	res.header('Content-Type', 'text/html'); 
-	res.send(fileContents);
+	fs.readFile('data.txt', function(err, data) {
+		res.header('Content-Type', 'text/html'); 
+		res.send(data);
+	});
 };
+
+
+
+app.get('/:filename', function(req, res) {
+	fs.readFile('./public/' + req.params.filename, function(err, data) {
+		res.header('Content-Type', 'text/html'); 
+		res.send(data);
+	});
+
+});
