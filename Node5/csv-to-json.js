59b3e4507739002a257a3a10889931e1e48711e2
Lauren Davis <londond9218@gmail.com>
2015-10-07 15:22:36 -0600

    fixed json output

--- Node5/csv-to-json.js
+++ Node5/csv-to-json.js
@@ -5,38 +5,27 @@ var jsonFileName = process.argv[3];
 
 var csvFileContents = fs.readFileSync(csvFileName, 'utf-8');
 
-// break the CSV into rows
-var rows = csvFileContents.split('\n');
+var rowSplit = csvFileContents.split('\n');
+console.log(rowSplit);
 
-// take out the first row, get that row, and split by comma
-var labels = rows.splice(0, 1)[0].split(',');
+var splitted = rowSplit.splice(0, 1)[0].split(',');
+console.log(splitted);
 
-// convert the remaining rows into objects
-var results = rows
-    // remove rows that are empty
-    .filter(function(row){
+var results = rowSplit.filter(function(row){
         return row !== '';
-    })
-    // replace string rows with an object representation
-    .map(function(row){
-        // Break the row up by commas
+    }).map(function(row){
         var columns = row.split(',');
-        // Create a temporary object
         var obj = {};
-        // For each of the columns, map them to a preset label
         for (var i = 0; i < columns.length; i++) {
-            var thisKey = labels[i];
+            var thisKey = splitted[i];
             var thisValue = columns[i];
             obj[thisKey] = thisValue;
         }
         return obj;
     });
 
-// Convert JS objects into JSON
 var resultsJson = JSON.stringify(results);
 
-// Print results to console
 console.log(resultsJson);
 
-// Store results in the file
 fs.writeFileSync(jsonFileName, resultsJson)
