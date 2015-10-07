fa4de61bd5133b0872437ae583644c5aa22c8f4b
Lauren Davis <londond9218@gmail.com>
2015-10-07 17:11:36 -0600

    started bonus, not finished

--- Problem4/main.js
+++ Problem4/main.js
@@ -1,19 +1,21 @@
-var count = 0; 
-var temp = '';
-
 var addNumbers = function(str) {
-	for (var i = 0; i < str.length; i++) {
-			temp = str[i];
-			i++;
-			while(Number(str[i])) {
-				temp += str[i];
-			}
-
-			count += Number(temp);
-		}
+  var strArr = [];
+  var numArr = [];
+	for (var i = 0; i <= str.length; i++) {
+    if(!isNaN(str[i])) {
+	     strArr.push(str[i]); 
+     }
+    else {
+      numArr.push(strArr.join('')); 
+      strArr = []; 
+    }
+  }
+  var count = 0; 
+  for (var i = 0; i < numArr.length; i++) {
+    count += Number(numArr[i]);
+  }
 	return count
 }
-
 
 
 var longestWord = function(str) {
@@ -33,5 +35,8 @@ var longestWord = function(str) {
 
 
 var averageStringNumbers = function(str) {
-
+  var answer = addNumbers(str); 
+  console.log(answer);
 }
+
+console.log(averageStringNumbers("Hello6 9World 2Nic8e D7ay"));
