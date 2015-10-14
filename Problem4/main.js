2cb177559390b1a77b3cee5e1f9aa5969b01e384
Lauren Davis <londond9218@gmail.com>
2015-10-14 17:54:13 -0600

    Problem 5 DONE!

--- Problem4/main.js
+++ Problem4/main.js
@@ -39,4 +39,48 @@ var averageStringNumbers = function(str) {
   console.log(answer);
 }
 
-console.log(averageStringNumbers("Hello6 9World 2Nic8e D7ay"));
+//console.log(averageStringNumbers("Hello6 9World 2Nic8e D7ay"));
+
+// PROBLEM 5
+
+var alphabetSoup = function(str) {
+  return str.split('').sort().join('');
+}
+
+var vowelCount = function(str) {
+  var count = 0; 
+  var newStr = str.toLowerCase();
+  for (var i = 0; i < str.length; i++) {
+    if(newStr[i] === 'a' || newStr[i] === 'e' || newStr[i] === 'i' || newStr[i] === 'o' || newStr[i] === 'u') {
+      count++
+    }
+  }
+  return count;
+
+}
+
+var coinDeterminer = function(num) {
+  var count = 0; 
+  do {
+    if (num >= 15) num-= 15;
+    else if (num >= 14) num -= 14;
+    else if (num >= 11) num -= 11;
+    else if (num >= 9) num -= 9;
+    else if (num >= 7) num -= 7;
+    else if (num >= 5) num -= 5;
+    else if (num >= 1) num -= 1;
+    count++;
+  } while (num > 0);
+  return count;
+
+  }
+
+
+
+
+
+
+
+
+
+
